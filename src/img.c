b29312aa3b23c220e80a605c7225d773e8c408da
Szymon Jesionek <sjesione@c2r4s2.42warsaw.pl>
2025-04-11 15:47:40 +0200

    HUGE improvement

--- src/img.c
+++ src/img.c
@@ -6,7 +6,7 @@
 /*   By: sjesione < sjesione@student.42warsaw.pl    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 22:01:37 by sjesione          #+#    #+#             */
-/*   Updated: 2025/04/11 15:03:45 by sjesione         ###   ########.fr       */
+/*   Updated: 2025/04/11 15:46:44 by sjesione         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,43 +23,41 @@ void	image_initialzer(t_map *map)
 	map->img.c_img = mlx_xpm_file_to_image(map->mlx,"images/collectible.xpm", &a, &a);
 	map->img.es_img = mlx_xpm_file_to_image(map->mlx, "images/escape.xpm",&a, &a);
 
-			if (!map->img.player || !map->img.e_img || !map->img.w_img ||
-				!map->img.c_img || !map->img.es_img)
-			{
-				ft_putstr_fd("Error\nOne or more images failed to load\n", 2);
-				exit(1);
-			}
+	if (!map->img.player || !map->img.e_img || !map->img.w_img ||
+		!map->img.c_img || !map->img.es_img)
+	{
+		ft_putstr_fd("Error\nOne or more images failed to load\n", 2);
+		exit(1);
+	}
+
 }
 
 void	map_maker(t_map *map, int i, int j)
 {
 	int	type;
 
-	type = map->mapp[i / PXL][j / PXL];
+	type = map->mapp[j / PXL][i / PXL];
 	if (type == 'C' || type == 'P' || type == 'E' || type == '0')
 		mlx_put_image_to_window(map->mlx, map->window, map->img.e_img, i, j);
 	if (type == 'C')
 		mlx_put_image_to_window(map->mlx, map->window, map->img.c_img, i, j);
 	else if (type == 'P')
-		mlx_put_image_to_window(map->mlx, map->window, map->img.player, i, j);
+		mlx_put_image_to_window(map->mlx, map->window, map->img.player, i,j);
 	else if (type == 'E')
 		mlx_put_image_to_window(map->mlx, map->window, map->img.es_img, i, j);
 	else if (type == '1')
 		mlx_put_image_to_window(map->mlx, map->window, map->img.w_img, i, j);
 }
-
 void	map_printer(t_map *map)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
+	i = -1;
+	while (++i < map->columns)
 	{
-		i = -1;
-		while (++i < map->rows)
-		{
-			j = -1;
-			while (++j < map->columns)
-				map_maker(map, i * PXL, j * PXL);
-		}
+		j = -1;
+		while (++j < map->rows)
+		map_maker(map,i*PXL,j*PXL);
 	}
 }
